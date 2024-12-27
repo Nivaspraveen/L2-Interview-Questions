71fb3410acb466958cae8fd303cd325deafbeed5
Nivaspraveen <nivaspraveen07@gmail.com>
2024-12-27 18:39:21 +0530

    added another test case for sort arr by freq

--- JAVASCRIPT/35_sort_array_by_frequency.js
+++ JAVASCRIPT/35_sort_array_by_frequency.js
@@ -1,6 +1,9 @@
 // 35. Sort the array elements in descending order according to their frequency of occurrence
 // Input : [ 2 2 3 4 5 12 2 3 3 3 12 ]
 // Output : [ 3 3 3 3 2 2 2 12 12 4 5 ]
+// Explanation : 3 occurred 4 times, 2 occurred 3 times, 12 occurred 2 times, 4 occurred 1time, 5 occurred 1 time 
+// Input : [ 0 -1 2 1 0 ]
+// Output : 0 0 -1 1 2 sort single occurrence elements in ascending order
 
 const sortByFrequency = arr => {
     const freqMap = new Map();
@@ -8,7 +11,7 @@ const sortByFrequency = arr => {
 
     const freqArr = Array.from(freqMap.entries());
     freqArr.sort((a, b) => {
-        if (b[1] === a[1]) return b[0] - a[0];
+        if (b[1] === a[1]) return a[1] === 1 ? a[0] - b[0] : b[0] - a[0];
         return b[1] - a[1];
     });
 
@@ -19,5 +22,8 @@ const sortByFrequency = arr => {
     return result;
 };
 
-const arr = [ 2, 2, 3, 4, 5, 12, 2, 3, 3, 3, 12 ];
-console.log(sortByFrequency(arr));
+let arr1 = [ 2, 2, 3, 4, 5, 12, 2, 3, 3, 3, 12 ];
+console.log(sortByFrequency(arr1));
+
+let arr2 = [0, -1, 2, 1, 0];
+console.log(sortByFrequency(arr2));
